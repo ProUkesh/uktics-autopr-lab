b47589147083a027f474732d0d16e9eb2f30c629
ProgrammerUkesh <ukeshbaniya7@gmail.com>
2026-02-15 14:02:58 -0600

    Introduce TypeScript Compile Error in App.tsx

---
+++ ui/src/App.tsx
@@ -0,0 +1,13 @@
+const __uktics_fail__: number = "boom";
+
+// Existing content of App.tsx
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, World!</h1>
+    </div>
+  );
+}
+
+export default App;
