5f6d93c1aef4f7c4490e8f95b8ef247376f7fa82
ProgrammerUkesh <ukeshbaniya7@gmail.com>
2026-02-15 21:22:45 -0600

    Add /api/healthz endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
+const express = require('express');
+const healthzRoute = require('./routes/healthz');
+
+const app = express();
+
+app.use('/api/healthz', healthzRoute);
+
 const __uktics_test__: number = 0;
 
 // Existing content of src/index.ts
 // ...
 
 void __uktics_test__;
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
