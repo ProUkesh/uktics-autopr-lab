b05b61ec5e7f74cb3fb5e3946723d127c32301f1
ProgrammerUkesh <ukeshbaniya7@gmail.com>
2026-02-15 21:22:42 -0600

    Add footer to UI

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>© 2023 Uktics. All rights reserved.</div>
+      <div>Follow us on social media!</div>
+    </footer>
+  );
+};
+
+export default Footer;
